6a5fd889804e539768218f9636793abf7f2cd007
Shaun Thornburgh <personal@shaunthornburgh.com>
2025-03-02 21:29:41 -0500

    eco system file

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: 'dennysgutierreznailspa',
+    script: 'npm',
+    args: 'start',
+    env: {
+      NUXT_BASE_URL: 'https://www.dennysgutierreznailspa.com'
+    }
+  }]
+};
